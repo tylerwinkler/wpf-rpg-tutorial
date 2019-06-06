4528d2648c5f11705eb2ebffce84f7be71077182
Tyler Winkler <tyler@tylerwinkler.com>
2019-06-06 16:09:28 -0400

    Lesson 3.3: Added Player class.

---
+++ WPFRPG/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFRPG
+{
+    class Player
+    {
+        public string Name { get; set; }
+
+        public string CharacterClass { get; set; }
+
+        public int Level { get; set; }
+
+        public int Experience { get; set; }
+
+        public int Health { get; set; }
+
+        public int Gold { get; set; }
+    }
+}
