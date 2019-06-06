79fd74e274e97b18f1e70dce0fc0f84e5a2c98e2
Tyler Winkler <tyler@tylerwinkler.com>
2019-06-06 16:11:30 -0400

    Lesson 3.4: Added GameSession class.

---
+++ WPFRPG/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFRPG
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+
+            CurrentPlayer.Name = "Tyler";
+            CurrentPlayer.Gold = 2000;
+        }
+    }
+}
